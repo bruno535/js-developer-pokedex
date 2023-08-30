289a71f7023d80d0868dec886b1254320c1d2cf8
bruno535 <bruno.henriqueds@outlook.com.br>
2023-08-29 22:45:17 -0300

    pokedex 2.0

--- README.md
+++ README.md
@@ -1,1 +1,49 @@
-# Trilha JS Developer - Pokedex
+# Pokedex Web App
+This is a simple web application that allows you to browse and view details of different Pokémon. It uses the PokéAPI to fetch Pokémon data and display it in a user-friendly format.
+
+## Getting Started
+To get started with this web app, follow these steps:
+
+1. Clone this repository to your local machine: 
+        
+        git clone <repository-url>
+
+2. Open the **index.html** file in your web browser to view the Pokedex.
+
+## Features
+- View a list of Pokémon with their names, numbers, types, and images.
+- Click on a Pokémon to see more details, including abilities and base stats.
+- Load more Pokémon as you scroll through the list.
+- Search for specific Pokémon using the search bar (feature to be added).
+
+## Usage
+1. View Pokémon List: Upon opening the app, you will see a list of Pokémon with their names, numbers, and types. Scroll down to load more Pokémon.
+
+2. **View Pokémon Details:** Click on any Pokémon in the list to view its details, including abilities and base stats.
+
+3. **Load More Pokémon:** Click the "Load More" button to load additional Pokémon.
+
+4. **Close Pokémon Details:** To close the Pokémon details modal, click the "X" button or click anywhere outside the modal.
+
+## Contributing
+If you would like to contribute to this project, please follow these steps:
+
+1. Fork the repository on GitHub.
+
+2. Clone your forked repository to your local machine:
+
+        git clone <your-forked-repo-url>
+
+3. Create a new branch for your feature or bug fix:
+
+        git checkout -b feature-name
+
+4. Make your changes and commit them with descriptive commit messages.
+
+5. Push your changes to your forked repository on GitHub:
+
+        git push origin feature-name
+
+6. Create a pull request to the original repository with a clear description of your changes.
+
+7. Your pull request will be reviewed, and if approved, your changes will be merged.
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,47 +1,120 @@
-const pokemonList = document.getElementById('pokemonList')
-const loadMoreButton = document.getElementById('loadMoreButton')
+const pokemonList = document.getElementById('pokemonList');
+const loadMoreButton = document.getElementById('loadMoreButton');
 
-const maxRecords = 151
-const limit = 10
+const maxRecords = 151;
+const limit = 10;
 let offset = 0;
 
 function convertPokemonToLi(pokemon) {
     return `
-        <li class="pokemon ${pokemon.type}">
-            <span class="number">#${pokemon.number}</span>
-            <span class="name">${pokemon.name}</span>
+      <li class="pokemon ${pokemon.type}" data-pokemon='${JSON.stringify(
+        pokemon
+    )}'>
+          <span class="number">#${pokemon.number}</span>
+          <span class="name">${pokemon.name}</span>
 
-            <div class="detail">
-                <ol class="types">
-                    ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
-                </ol>
+          <div class="detail">
+              <ol class="types">
+                  ${pokemon.types
+            .map((type) => `<li class="type ${type}">${type}</li>`)
+            .join('')}
+              </ol>
 
-                <img src="${pokemon.photo}"
-                     alt="${pokemon.name}">
-            </div>
-        </li>
-    `
+              <img src="${pokemon.photo}" alt="${pokemon.name}">
+          </div>
+      </li>
+  `;
+}
+
+pokemonList.addEventListener('click', (event) => {
+    const listItem = event.target.closest('.pokemon');
+    if (listItem) {
+        const pokemonData = JSON.parse(listItem.getAttribute('data-pokemon'));
+        displayPokemonModal(pokemonData);
+    }
+});
+function displayPokemonModal(pokemon) {
+    const modal = document.getElementById('myModal');
+    const modalContent = document.getElementById('modalDetails');
+
+    modalContent.innerHTML = `
+  <div>
+    <div class="titleCard ${pokemon.type}">
+      <h2>${pokemon.name[0].toUpperCase() + pokemon.name.slice(1)}  Number: #${pokemon.number
+        }</h2>
+    </div>
+
+    <div class="container">
+      <div class="container-box">
+        <div class="typeCard ${pokemon.type}">  
+          <p class="abilities">Type(s): ${pokemon.types.join(', ')}</p>
+          <p class="type">Abilities: ${pokemon.abilities.join(', ')}</span>
+        </div>   
+
+        <div class="statsCard">
+          <div class="stat ${pokemon.type}">
+            <p>${pokemon.statsName[0]}</p>
+            <p class="number">${pokemon.statsValue[0]}</p>
+          </div>
+          <div class="stat ${pokemon.type}"">
+            <p>${pokemon.statsName[1]}</p>
+            <p class="number">${pokemon.statsValue[1]}</p>
+          </div>
+          <div class="stat ${pokemon.type}"">
+            <p>${pokemon.statsName[2]}</p>
+            <p class="number">${pokemon.statsValue[2]}</p>
+          </div>
+          <div class="stat ${pokemon.type}"">
+            <p>${pokemon.statsName[3]}</p>
+            <p class="number">${pokemon.statsValue[3]}</p>
+          </div>
+          <div class="stat ${pokemon.type}"">
+            <p>${pokemon.statsName[4]}</p>
+            <p class="number">${pokemon.statsValue[4]}</p>
+          </div>
+        </div>
+      </div>
+
+      <div class="imageCard">
+        <img src="${pokemon.photo}" alt="${pokemon.name}">
+      </div>
+    </div>
+  </div>
+  `;
+
+    modal.style.display = 'block';
+
+    const closeButton = document.querySelector('.close');
+    closeButton.addEventListener('click', () => {
+        modal.style.display = 'none';
+    });
+
+    window.addEventListener('click', (event) => {
+        if (event.target === modal) {
+            modal.style.display = 'none';
+        }
+    });
 }
 
 function loadPokemonItens(offset, limit) {
     pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
-        const newHtml = pokemons.map(convertPokemonToLi).join('')
-        pokemonList.innerHTML += newHtml
-    })
+        const newHtml = pokemons.map(convertPokemonToLi).join('');
+        pokemonList.innerHTML += newHtml;
+    });
 }
 
-loadPokemonItens(offset, limit)
+loadPokemonItens(offset, limit);
 
 loadMoreButton.addEventListener('click', () => {
-    offset += limit
-    const qtdRecordsWithNexPage = offset + limit
+    offset += limit;
+    const qtdRecordsWithNexPage = offset + limit;
 
     if (qtdRecordsWithNexPage >= maxRecords) {
-        const newLimit = maxRecords - offset
-        loadPokemonItens(offset, newLimit)
+        const newLimit = maxRecords - offset;
+        loadPokemonItens(offset, newLimit);
 
-        loadMoreButton.parentElement.removeChild(loadMoreButton)
+        loadMoreButton.parentElement.removeChild(loadMoreButton);
     } else {
-        loadPokemonItens(offset, limit)
+        loadPokemonItens(offset, limit);
     }
-})
+});
